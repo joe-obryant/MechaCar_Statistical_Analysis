1d81591b00dedaed79a7a80022555d5f93cb88bb
joe-obryant <josephobryant@ucdenver.edu>
2023-02-15 14:45:48 -0700

    final

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -18,3 +18,9 @@ total_summary
 lot_summary <- suscoil.table %>% group_by(Manufacturing_Lot) %>% summarise(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups='keep')
 lot_summary
 
+# Part 3
+t.test(suscoil.table$PSI, mu=1500)
+
+t.test(subset(suscoil.table, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+t.test(subset(suscoil.table, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+t.test(subset(suscoil.table, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
